b39ee80bdea167ab38b07a34418b50c30f9a6770
thomp <thomp@protonmail.com>
2018-05-11 13:59:27 -0500

    creating Account object

---
+++ src/com/maat/Account.java
@@ -0,0 +1,16 @@
+package com.maat;
+
+/**
+ * Created by mthom on 5/11/2018.
+ */
+public class Account {
+    private int interestRate;
+    private int penalty;
+    private int balance;
+
+
+
+    public Account() {
+        this.interestRate = 5;
+    }
+}
--- src/com/maat/Main.java
+++ src/com/maat/Main.java
@@ -2,6 +2,8 @@ package com.maat;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, world!");
